54756a3927aa8d0f332ff11d91b1be1786b1772c
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-09-09 00:08:45 +0530

    Flask Basic

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('Home.html')
+
+@app.route('/about')
+def About():
+    return render_template('About.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
